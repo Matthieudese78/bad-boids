f352f90877e4ad89a92d4cd52e5c680271d76697
matthieu <66738778+Matthieudese78@users.noreply.github.com>
2025-06-30 21:47:54 +0200

    commit

--- record_fixture.py
+++ record_fixture.py
@@ -1,12 +1,18 @@
+# %%
 from copy import deepcopy
 
 import yaml
 
-from boids import boids
+from boids.boids import boids, update_boids
+import numpy as np
 
-before = deepcopy(boids.boids)
-boids.update_boids(boids.boids)
-after = boids.boids
+# %%
+print(np.shape(boids))
+
+before = deepcopy(boids)
+after = update_boids(before)
 fixture = {"before": before, "after": after}
 with open("fixture.yml", "w") as fixture_file:
     fixture_file.write(yaml.safe_dump(fixture))
+
+# %%
--- src/boids/boids.py
+++ src/boids/boids.py
@@ -1,10 +1,18 @@
+# %%
 """An amelioration of the deliberately bad implementation of [Boids](http://dl.acm.org/citation.cfm?doid=37401.37406)."""
+
+from future import annotations
+from itertools import product
 
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib import animation
 
 BIRDS_POPULATION = 50
+REPELLING_DISTANCE = 100.0
+ATTRACTING_DISTANCE = 10000.0
+ATTRACTION_RATE = 0.125
+MIDDLE_ATTRACTION_RATE = 0.01
 
 # %%
 # Deliberately terrible code for teaching purposes
@@ -14,55 +22,57 @@ boids_y = [np.random.uniform(300.0, 600.0) for x in range(BIRDS_POPULATION)]
 boid_x_velocities = [np.random.uniform(0, 10.0) for x in range(BIRDS_POPULATION)]
 boid_y_velocities = [np.random.uniform(-20.0, 20.0) for x in range(BIRDS_POPULATION)]
 boids = (boids_x, boids_y, boid_x_velocities, boid_y_velocities)
+print(np.shape(boids))
+
+position = np.array([boids[0], boids[1]]).T
+velocity = np.array([boids[1], boids[2]]).T
 
 
-def update_boids(boids: tuple):
-    xs, ys, xvs, yvs = boids
-    delta_xvs = np.zeros_like(xs)
-    delta_yvs = np.zeros_like(xs)
+# %%
+class Boids:
+    """A class to represent the boids simulation."""
 
-    # Fly towards the middle
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            delta_xvs[i] = delta_xvs[i] + (xs[j] - xs[i]) * 0.01 / len(xs)
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            delta_yvs[i] = delta_yvs[i] + (ys[j] - ys[i]) * 0.01 / len(xs)
-    # Fly away from nearby boids
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 100:
-                delta_xvs[i] = delta_xvs[i] + (xs[i] - xs[j])
-                delta_yvs[i] = delta_yvs[i] + (ys[i] - ys[j])
-    # Try to match speed with nearby boids
-    for i in range(len(xs)):
-        for j in range(len(xs)):
-            if (xs[j] - xs[i]) ** 2 + (ys[j] - ys[i]) ** 2 < 10000:
-                delta_xvs[i] = delta_xvs[i] + (xvs[j] - xvs[i]) * 0.125 / len(xs)
-                delta_yvs[i] = delta_yvs[i] + (yvs[j] - yvs[i]) * 0.125 / len(xs)
-    # Update velocities
-    for i in range(len(xs)):
-        xvs[i] = xvs[i] + delta_xvs[i]
-        yvs[i] = yvs[i] + delta_yvs[i]
-    # Move according to velocities
-    for i in range(len(xs)):
-        xs[i] = xs[i] + xvs[i]
-        ys[i] = ys[i] + yvs[i]
+    def __init__(self, position: np.ndarray, velocity: np.ndarray):
+        self.position = position
+        self.velocity = velocity
 
+    def update_boids(self) -> None:
+        """Update the positions and velocities of the boids."""
+        position = self.position
+        velocity = self.velocity
+        delta_velocity = np.zeros_like(velocity)
 
-figure = plt.figure()
-axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
-scatter = axes.scatter(boids[0], boids[1])
+        for i, j in product(range(BIRDS_POPULATION), repeat=2):
+            # Fly towards the middle
+            delta_velocity[i] = (
+                delta_velocity[i] + (position[j] - position[i]) * MIDDLE_ATTRACTION_RATE / BIRDS_POPULATION
+            )
+
+            # Fly away from nearby boids
+            if np.linalg.norm(position[j] - position[i]) ** 2 < REPELLING_DISTANCE:
+                delta_velocity[i] = delta_velocity[i] + (position[i] - position[j])
+
+            # Try to match speed with nearby boids
+            if np.linalg.norm(position[j] - position[i]) ** 2 < ATTRACTING_DISTANCE:
+                delta_velocity[i] = delta_velocity[i] + (velocity[j] - velocity[i]) * ATTRACTION_RATE / BIRDS_POPULATION
+
+        # Update positions & velocities
+        self.velocity = self.velocity + delta_velocity
+        self.position = self.position + velocity
 
 
-def ANIMATE(frame):
-    update_boids(boids)
-    scatter.set_offsets(list(zip(boids[0], boids[1])))
+def animate(boids: Boids) -> None:
+    boids.update_boids()
+    scatter.set_offsets(list(zip(boids.position[:, 0], boids.position[:, 1])))
 
-
-anim = animation.FuncAnimation(figure, ANIMATE, frames=50, interval=50)
 
 if __name__ == "__main__":
+    boids = Boids(position, velocity)
+    figure = plt.figure()
+    axes = plt.axes(xlim=(-500, 1500), ylim=(-500, 1500))
+    scatter = axes.scatter(boids.position[:, 0], boids.position[:, 1])
+
+    anim = animation.FuncAnimation(figure, animate, frames=50, blit=True)
     plt.show()
 
 # %%
